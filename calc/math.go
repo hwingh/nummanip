3eeb68da271a1540d51bdfb94a2364fce9befbd1
hwingh <hwingh@yeah.net>
2019-06-24 16:43:47 +0800

    code lint

--- calc/math.go
+++ calc/math.go
@@ -10,12 +10,11 @@ func Add(numbers ...int) (int, error) {
 	if len(numbers) < 2 {
 		errorMessages := color.RedString("provide more than 2 numbers")
 		return sum, errors.New(errorMessages)
-	} else {
-		for _, num := range numbers {
-			sum = sum + num
-		}
-
-		return sum, nil
 	}
 
+	for _, num := range numbers {
+		sum = sum + num
+	}
+
+	return sum, nil
 }
