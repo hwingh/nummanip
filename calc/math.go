ad17ba4e678371c8979e1070f52a49e2e4f92992
hwingh <hwingh@yeah.net>
2019-06-24 16:41:14 +0800

    Add support for colorful message

--- calc/math.go
+++ calc/math.go
@@ -1,13 +1,15 @@
 package calc
 
 import "errors"
+import "github.com/fatih/color"
 
 // Add return sum of two integers
 func Add(numbers ...int) (int, error) {
 	sum := 0
 
 	if len(numbers) < 2 {
-		return sum, errors.New("provide more than 2 numbers")
+		errorMessages := color.RedString("provide more than 2 numbers")
+		return sum, errors.New(errorMessages)
 	} else {
 		for _, num := range numbers {
 			sum = sum + num
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/hwingh/nummanip/v2
 
 go 1.12
+
+require (
+	github.com/fatih/color v1.7.0
+	github.com/hwingh/nummanip v1.0.2 // indirect
+	github.com/mattn/go-colorable v0.1.2 // indirect
+	golang.org/x/sys v0.0.0-20190621203818-d432491b9138 // indirect
+)
